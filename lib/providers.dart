1dd82dcccb4e128ca8f56d186e50109412a57672
ShashankSumanth <145141799+ShashankSumanth@users.noreply.github.com>
2024-02-27 18:12:26 +0530

    Update providers.dart

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,119 +1,20 @@
-import 'dart:convert';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class MovieListProvider extends ChangeNotifier {
-  Map<String, dynamic> _movieList = {};
-  Map<String, dynamic> get movieList => _movieList;
 
-  List<dynamic> _movieInfo = [];
-  List<dynamic> get movieInfo => _movieInfo;
+final movieListProvider = FutureProvider.family<Map<String, dynamic>, String>((ref, String utitle) async {
+  Map<String, dynamic> movieList = {};
+  Map<String, dynamic> ImageList = {};
 
-  bool _isLoading = false;
-  bool get isLoading => _isLoading;
-
-  Future<int> returnList(String utitle) async {
-    if (utitle == " "){
-      const url = "https://movies-tv-shows-database.p.rapidapi.com/";
-      final uri = Uri.parse(url).replace(queryParameters: {'page': '1'});
-        final response = await http.get(
-          uri,
-          headers: {
-            'Type': "get-nowplaying-movies",
-            'X-RapidAPI-Key': "468ea9dcc0msh13ec1d1d0a74434p180135jsn06e756bcb814",
-            'X-RapidAPI-Host': "movies-tv-shows-database.p.rapidapi.com"
-          }
-        );
-        if (response.statusCode == 200) {
-          final body = response.body;
-          if (body.isNotEmpty) {
-            _movieList = jsonDecode(body);
-            for (int counter = 0; counter<_movieList['results'];counter++){
-              _movieList['movie_results'][counter]['favorite'] = false;
-            }
-            _isLoading = false;
-            notifyListeners();
-          }
-        }
-        return 1;
-    }
+  if (utitle == ""){
+    print('1.');
     const url = "https://movies-tv-shows-database.p.rapidapi.com/";
-    final uri = Uri.parse(url).replace(queryParameters: {'title': utitle});
-      final response = await http.get(
-        uri,
-        headers: {
-          'Type': "get-movies-by-title",
-          'X-RapidAPI-Key': "468ea9dcc0msh13ec1d1d0a74434p180135jsn06e756bcb814",
-          'X-RapidAPI-Host': "movies-tv-shows-database.p.rapidapi.com"
-        }
-      );
-      if (response.statusCode == 200) {
-        final body = response.body;
-        if (body.isNotEmpty) {
-          _movieList = jsonDecode(body);
-          for (int counter = 0; counter<(_movieList['search_results']).toInt();counter++){
-            _movieList['movie_results'][counter]['favorite'] = false;
-          }
-          _isLoading = false;
-          notifyListeners();
-        }
-      }
-      return 1;
-  }
-
-  void toggleBookmark(int index){
-    _movieList['movie_results'][index]['favorite'] = !(_movieList['movie_results'][index]['favorite']);
-    notifyListeners();
-  }
-
-  Future<int> MovieImageURLProvider() async {
-    _movieInfo = [];
-    Map<String, dynamic> InfoList = {};
-    for (var movie in _movieList['movie_results']){
-      final imdbId = movie['imdb_id'];
-      const url = "https://movies-tv-shows-database.p.rapidapi.com/";
-      final uri = Uri.parse(url).replace(queryParameters: {'movieid': imdbId});
-      final response = await http.get(
-        uri,
-        headers: {
-          'Type': "get-movies-images-by-imdb",
-          'X-RapidAPI-Key': "468ea9dcc0msh13ec1d1d0a74434p180135jsn06e756bcb814",
-          'X-RapidAPI-Host': "movies-tv-shows-database.p.rapidapi.com"
-        }
-      );
-        if (response.statusCode == 200) {
-          final body = response.body;
-          if (body.isNotEmpty) {
-            InfoList = jsonDecode(body);
-          }
-        }
-      if (InfoList['poster'] != ''){
-        _movieInfo.add(InfoList['poster']);
-      } else{
-        _movieInfo.add('https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=');
-      }
-    }
-    print(_movieInfo);
-    notifyListeners();
-    return 1;
-  }
-}
-
-class MovieInformationProvider extends ChangeNotifier{
-  
-  Map<String, dynamic> _movieDets = {};
-  Map<String, dynamic> get movieDets => _movieDets;
-
-  List<String> _fanArtURL = [];
-  List<String> get fanArtURL => _fanArtURL;
-  
-  Future<int> movieDetails(String? movieID) async {
-    const url = "https://movies-tv-shows-database.p.rapidapi.com/";
-    final uri = Uri.parse(url).replace(queryParameters: {'movieid': movieID});
-    final response =  await http.get(
+    final uri = Uri.parse(url).replace(queryParameters: {'page': '1'});
+    final response = await http.get(
       uri,
       headers: {
-        'Type': "get-movie-details",
+        'Type': "get-nowplaying-movies",
         'X-RapidAPI-Key': "468ea9dcc0msh13ec1d1d0a74434p180135jsn06e756bcb814",
         'X-RapidAPI-Host': "movies-tv-shows-database.p.rapidapi.com"
       }
@@ -121,19 +22,35 @@ class MovieInformationProvider extends ChangeNotifier{
       if (response.statusCode == 200) {
         final body = response.body;
         if (body.isNotEmpty) {
-          _movieDets = jsonDecode(body);
-          notifyListeners();
-          return 1;
+          movieList = jsonDecode(body);
         }
       }
-      return 1;
-  }
-
-  Future<int> movieImageURLProvider(String? movieID) async {
-    _fanArtURL = [];
-    Map<String, dynamic> InfoList = {};
+    print(movieList);
+  }else{
+    print('2');
     const url = "https://movies-tv-shows-database.p.rapidapi.com/";
-    final uri = Uri.parse(url).replace(queryParameters: {'movieid': movieID});
+    final uri = Uri.parse(url).replace(queryParameters: {'title': utitle});
+    final response = await http.get(
+      uri,
+      headers: {
+        'Type': "get-movies-by-title",
+        'X-RapidAPI-Key': "468ea9dcc0msh13ec1d1d0a74434p180135jsn06e756bcb814",
+        'X-RapidAPI-Host': "movies-tv-shows-database.p.rapidapi.com"
+        }
+      );
+      if (response.statusCode == 200) {
+        final body = response.body;
+        if (body.isNotEmpty) {
+          movieList = jsonDecode(body);
+        }
+      }
+      print(movieList);
+  };
+  for (int counter = 0; counter<(movieList['movie_results']).length; counter++){
+    movieList['movie_results'][counter]['favorite'] = false;
+    String imdbID = movieList['movie_results'][counter]['imdb_id'];
+    const url = "https://movies-tv-shows-database.p.rapidapi.com/";
+    final uri = Uri.parse(url).replace(queryParameters: {'movieid': imdbID});
     final response = await http.get(
       uri,
       headers: {
@@ -141,15 +58,62 @@ class MovieInformationProvider extends ChangeNotifier{
         'X-RapidAPI-Key': "468ea9dcc0msh13ec1d1d0a74434p180135jsn06e756bcb814",
         'X-RapidAPI-Host': "movies-tv-shows-database.p.rapidapi.com"
       }
-      );
-        if (response.statusCode == 200) {
-          final body = response.body;
-          if (body.isNotEmpty) {
-            InfoList = jsonDecode(body);
-          }
-        }
-    _fanArtURL = (InfoList['fanart']).split(',');
-    notifyListeners();
-    return 1;
+    );
+    if (response.statusCode == 200) {
+      final body = response.body;
+      if (body.isNotEmpty) {
+        ImageList = jsonDecode(body);
+      }
+    }
+    if (ImageList['poster'] != ""){
+      movieList['movie_results'][counter]['poster'] = ImageList['poster'];
+    } else {
+      movieList['movie_results'][counter]['poster'] = 'https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=';
+    }
   }
-}
+  return movieList;
+});
+
+final movieDetailsProvider = FutureProvider.family<Map<String, dynamic>, String?>((ref, String? movieID) async{
+  print('3.');
+  Map<String, dynamic> movieDets = {};
+  Map<String, dynamic> InfoList = {};
+  List<String> fanArtURL = [];
+  const url1 = "https://movies-tv-shows-database.p.rapidapi.com/";
+  final uri1 = Uri.parse(url1).replace(queryParameters: {'movieid': movieID});
+  final response1 =  await http.get(
+    uri1,
+    headers: {
+      'Type': "get-movie-details",
+      'X-RapidAPI-Key': "468ea9dcc0msh13ec1d1d0a74434p180135jsn06e756bcb814",
+      'X-RapidAPI-Host': "movies-tv-shows-database.p.rapidapi.com"
+    }
+  );
+  if (response1.statusCode == 200) {
+    final body1 = response1.body;
+    if (body1.isNotEmpty) {
+      movieDets = jsonDecode(body1);
+    }
+  }
+
+  const url2 = "https://movies-tv-shows-database.p.rapidapi.com/";
+  final uri2 = Uri.parse(url2).replace(queryParameters: {'movieid': movieID});
+  final response2 = await http.get(
+    uri2,
+    headers: {
+      'Type': "get-movies-images-by-imdb",
+      'X-RapidAPI-Key': "468ea9dcc0msh13ec1d1d0a74434p180135jsn06e756bcb814",
+      'X-RapidAPI-Host': "movies-tv-shows-database.p.rapidapi.com"
+    }
+    );
+    if (response2.statusCode == 200) {
+      final body = response2.body;
+      if (body.isNotEmpty) {
+        InfoList = jsonDecode(body);
+      }
+    }
+  fanArtURL = (InfoList['fanart']).split(',');
+  movieDets['fanart'] = fanArtURL;
+  print(movieDets);
+  return movieDets;
+});
